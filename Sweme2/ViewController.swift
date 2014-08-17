7861d3796c7bba3ac864e51826a502f43bb8ad00
knj4484 <knj4484@gmail.com>
2014-08-17 22:39:26 +0900

    Initial Commit

---
+++ Sweme2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sweme2
+//
+//  Created by YOSHIHASHI Kenji on 8/17/14.
+//  Copyright (c) 2014 mannan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
