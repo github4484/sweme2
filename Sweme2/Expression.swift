a0d0bae1644080e26b3b1e58a0f2f5fcad6e85c8
knj4484 <knj4484@gmail.com>
2015-08-06 08:34:14 +0900

    formatted

--- Sweme2/Expression.swift
+++ Sweme2/Expression.swift
@@ -1,33 +1,69 @@
-class Expression { func toString() -> String { return "#E" } }
-class Nil : Expression { override func toString() -> String { return "#N" } }
+class Expression {
+    func toString() -> String { return "#E" }
+}
+
+class Nil : Expression {
+    override func toString() -> String {
+        return "#N"
+    }
+}
+
 class Symbol : Expression {
     let name: String
-    init(name: String){ self.name = name }
-    override func toString() -> String { return "symbol(" + self.name + ")" }
+    init(name: String){
+        self.name = name
+    }
+    
+    override func toString() -> String {
+        return "symbol(" + self.name + ")"
+    }
 }
+
 class Boolean : Expression {
     let value: Bool
-    init(value: Bool) { self.value = value }
-    override func toString() -> String { return self.value ? "#T" : "#F" }
+    init(value: Bool) {
+        self.value = value
+    }
+
+    override func toString() -> String {
+        return self.value ? "#T" : "#F"
+    }
 }
+
 class Number : Expression {
     let value: Int
-    init(value: Int){ self.value = value }
-    override func toString() -> String { return String(self.value) }
+    init(value: Int) {
+        self.value = value
+    }
+    override func toString() -> String {
+        return String(self.value)
+    }
 }
+
 class List : Expression {
     var es: [Expression] = [] // es stands for ElementS
-    init(es: [Expression]) { self.es = es }
-    override func toString() -> String { return "(" + join(" ", map(es) { $0.toString() }) + ")" }
+    
+    init(es: [Expression]) {
+        self.es = es
+    }
+
+    override func toString() -> String {
+        return "(" + join(" ", map(es) { $0.toString() }) + ")"
+    }
 }
+
 class Procedure : Expression {
     var params: List
     var body: List
     var lexicalEnv: Environment
+
     init(params: List, body: List, lexicalEnv: Environment){
         self.params = params
         self.body = body
         self.lexicalEnv = lexicalEnv
     }
-    override func toString() -> String { return "\\(\(params.toString()) \(body.toString()))" }
+
+    override func toString() -> String {
+        return "\\(\(params.toString()) \(body.toString()))"
+    }
 }
